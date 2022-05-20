39f3eac20f3f0f1abe0bb0253b4dd04c87f29afe
Guilhem Niot <guilhem@gniot.fr>
2022-05-20 10:40:03 +0200

    Add a file to easily reproduce paper experiments

--- paper_experiments.sh
+++ paper_experiments.sh
@@ -6,11 +6,11 @@ DATASETS=(
     "dblp"
 )
 
-
 for count in 15 30 60
 do
     for ((i = 0; i < ${#DATASETS[@]}; i++))
     do
+        python3 run.py --algorithm "hnsw(nmslib)*" --dataset ${DATASETS[$i]}-jaccard --parallelism 5 -k $count
         python3 plot.py --dataset ${DATASETS[$i]}-jaccard --count $count -x qps -y k-nn --output results/paper/${DATASETS[$i]}-k${count}-recall.png
         python3 plot.py --dataset ${DATASETS[$i]}-jaccard --count $count -x qps -y quality --output results/paper/${DATASETS[$i]}-k${count}-quality.png
     done
